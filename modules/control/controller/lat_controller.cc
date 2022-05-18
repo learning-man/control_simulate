d655d53ef633f2c9737d905fcb1c0c4804e5e12f
learning-man <40049278+learning-man@users.noreply.github.com>
2022-05-18 10:40:04 +0800

    Update lat_controller.cc

--- modules/control/controller/lat_controller.cc
+++ modules/control/controller/lat_controller.cc
@@ -210,16 +210,16 @@ Status LatController::Init(std::shared_ptr<DependencyInjector> injector,
    0.0, ((lr * cr - lf * cf) / i_z) / v, (l_f * c_f - l_r * c_r) / i_z,
    (-1.0 * (l_f^2 * c_f + l_r^2 * c_r) / i_z) / v;]
   */
-  matrix_a_(0, 1) = (1);
-  matrix_a_(1, 2) = ((cf_ + cr_)/mass_);
-  matrix_a_(2, 3) = (1);
-  matrix_a_(3, 2) = ((cf_*lf_ - cr_*lr_)/iz_);
+  matrix_a_(0, 1) = 1.0;
+  matrix_a_(1, 2) = (cf_ + cr_)/mass_;
+  matrix_a_(2, 3) = 1.0;
+  matrix_a_(3, 2) = (cf_*lf_ - cr_*lr_)/iz_;
 
   matrix_a_coeff_ = Matrix::Zero(matrix_size, matrix_size);
-  matrix_a_coeff_(1, 1) = (-(cf_ + cr_)/mass_);
-  matrix_a_coeff_(1, 3) = ((-cf_*lf_ + cr_*lr_)/mass_);
-  matrix_a_coeff_(3, 1) = (-(cf_*lf_ - cr_*lr_)/iz_);
-  matrix_a_coeff_(3, 3) = (-(cf_*lf_*lf_ + cr_*lr_*lr_)/iz_);
+  matrix_a_coeff_(1, 1) = -(cf_ + cr_)/mass_;
+  matrix_a_coeff_(1, 3) = (-cf_*lf_ + cr_*lr_)/mass_;
+  matrix_a_coeff_(3, 1) = -(cf_*lf_ - cr_*lr_)/iz_;
+  matrix_a_coeff_(3, 3) = -1.0*(cf_*lf_*lf_ + cr_*lr_*lr_)/iz_;
 
   /*
   b = [0.0, c_f / m, 0.0, l_f * c_f / i_z]^T
@@ -519,7 +519,7 @@ Status LatController::ComputeControlCommand(
       }
     }
   }
-  steer_angle = (steer_angle_feedback) + (steer_angle_feedforward) +
+  steer_angle = steer_angle_feedback + steer_angle_feedforward +
                 steer_angle_feedback_augment;
 
   // Compute the steering command limit with the given maximum lateral
@@ -743,7 +743,7 @@ void LatController::UpdateMatrixCompound() {
 
 double LatController::ComputeFeedForward(double ref_curvature) const {
   const double kv =
-      ((lr_*mass_)/(2*cf_*wheelbase_)) - ((lf_*mass_)/(2*cr_*wheelbase_));
+      lr_ * mass_/ 2 / cf_ / wheelbase_ - lf_ * mass_/ 2 / cr_ / wheelbase_;
 
   // Calculate the feedforward term of the lateral controller; then change it
   // from rad to %
@@ -755,10 +755,10 @@ double LatController::ComputeFeedForward(double ref_curvature) const {
                                   steer_single_direction_max_degree_ * 100;
   } else {
     steer_angle_feedforwardterm =
-        ((wheelbase_*ref_curvature) + (kv*v*v*ref_curvature) -
-         (matrix_k_(0,2)) *
-             ((lr_/(2*cf_)) -
-              ((lf_*mass_*v*v*ref_curvature)/(2*cr_*wheelbase_))) *
+        wheelbase_*ref_curvature + kv*v*v*ref_curvature -
+         matrix_k_(0,2) *
+             (lr_/(2*cf_) -
+              (lf_*mass_*v*v*ref_curvature)/(2*cr_*wheelbase_) *
         180 / M_PI * steer_ratio_ / steer_single_direction_max_degree_ * 100;
   }
 
